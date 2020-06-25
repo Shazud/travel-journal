6edb3315b2608dc105c6ccb9f647ed43c4e737bd
David-Daniel Pava <david.pava@protonmail.com>
2020-06-25 14:45:09 +0300

    started recycle view dev

--- app/src/main/java/com/ddp/tj/trip/TripAdapter.java
+++ app/src/main/java/com/ddp/tj/trip/TripAdapter.java
@@ -1,5 +1,37 @@
 package com.ddp.tj.trip;
 
-public class TripAdapter {
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class TripAdapter extends RecyclerView.Adapter<TripAdapter.TripViewHolder> {
+    private ArrayList<Trip> data;
+    private ArrayList<Trip> dataAll;
+
+    @NonNull
+    @Override
+    public TripViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TripViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class TripViewHolder extends RecyclerView.ViewHolder {
+
+        public TripViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }

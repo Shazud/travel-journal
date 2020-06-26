a8b7d809f39b3d77348f15a0e9b4d8c79f12a4da
David-Daniel Pava <david.pava@protonmail.com>
2020-06-26 18:11:38 +0300

    take pickture implemented

--- app/src/main/java/com/ddp/tj/fragment/EditFragment.java
+++ app/src/main/java/com/ddp/tj/fragment/EditFragment.java
@@ -2,7 +2,6 @@ package com.ddp.tj.fragment;
 
 import android.Manifest;
 import android.app.DatePickerDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -22,13 +21,10 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RatingBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.ddp.tj.R;
@@ -56,7 +52,10 @@ public class EditFragment extends Fragment {
     private FloatingActionButton deleteButton;
 
     private static final int IMAGE_PICK_CODE = 1000;
-    private static final int PERMISSION_CODE = 1001;
+    private static final int PERMISSION_CODE_GALLERY = 1001;
+    private static final int TAKE_PICK_CODE = 1002;
+    private static final int PERMISSION_CODE_CAMERA = 1003;
+
 
     private ArrayList<Trip> data;
 
@@ -285,7 +284,7 @@ public class EditFragment extends Fragment {
                 if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
                     if(getActivity().checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
                         String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
-                        requestPermissions(permissions, PERMISSION_CODE);
+                        requestPermissions(permissions, PERMISSION_CODE_GALLERY);
                     }
                     else{
                         pickImageFromGallery();
@@ -297,7 +296,27 @@ public class EditFragment extends Fragment {
             }
         });
 
+        takePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(getActivity().checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
+                    String[] permissions = {Manifest.permission.CAMERA};
+                    requestPermissions(permissions, PERMISSION_CODE_CAMERA);
+                }
+                else{
+                    takePhotoCamera();
+                }
+            }
+        });
+
         return view;
+    }
+
+    private void takePhotoCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getActivity().getPackageManager())!=null){
+            startActivityForResult(intent, TAKE_PICK_CODE);
+        }
     }
 
     private void pickImageFromGallery() {
@@ -309,9 +328,17 @@ public class EditFragment extends Fragment {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode){
-            case PERMISSION_CODE:
+            case PERMISSION_CODE_GALLERY:
                 if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                     pickImageFromGallery();
+                }
+                else{
+                    Toast.makeText(getContext(), "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case PERMISSION_CODE_CAMERA:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    takePhotoCamera();
                 }
                 else{
                     Toast.makeText(getContext(), "Permission denied", Toast.LENGTH_SHORT).show();
@@ -331,6 +358,13 @@ public class EditFragment extends Fragment {
             catch (Exception ignored){
                 //Toast.makeText(getContext(), "exception",Toast.LENGTH_SHORT).show();
             }
+        }
+
+        if(resultCode == getActivity().RESULT_OK && requestCode == TAKE_PICK_CODE){
+            Bundle extras = data.getExtras();
+            Bitmap image = (Bitmap)extras.get("data");
+            imageView.setImageBitmap(image);
+            this.trip.setPicture(image);
         }
     }
 }

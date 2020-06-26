2cfccdae614329ae114fdddd2adbbba2359eb975
David-Daniel Pava <david.pava@protonmail.com>
2020-06-27 01:24:01 +0300

    implemented contact page with email event

--- app/src/main/java/com/ddp/tj/fragment/ContactFragment.java
+++ app/src/main/java/com/ddp/tj/fragment/ContactFragment.java
@@ -1,5 +1,6 @@
 package com.ddp.tj.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,11 +11,23 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.ddp.tj.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ContactFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_contact, container, false);
+        View view = inflater.inflate(R.layout.fragment_contact, container, false);
+        FloatingActionButton fab = view.findViewById(R.id.contact_fragment_mail_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_EMAIL, "contact@ddp.traverjournal.com");
+                intent.setType("message/rfc822");
+                startActivity(Intent.createChooser(intent, "Send using:"));
+            }
+        });
+        return view;
     }
 }
